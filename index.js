e5c2cfd9d2b79a0f3b57c01e2e64159d1522bb5f
Takashi Izuki <148468601+izukioka@users.noreply.github.com>
2024-05-27 21:45:52 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const axios = require('axios');
+const cheerio = require('cheerio')
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.enable("trust proxy");
@@ -78,7 +79,7 @@ async function blackboxAIChat(message) {
   }
 }
 
-async function GPT(message) {
+async function GPT(text) {
   return new Promise(async (resolve, reject) => {
     axios("https://www.chatgptdownload.org/wp-json/mwai-ui/v1/chats/submit", {
       "headers": {
@@ -94,7 +95,7 @@ async function GPT(message) {
         "messages": [{
           "id": "fkzhaikd7vh",
           "role": "assistant",
-          "content": "Perkenalkan Dirimu Dengan Nama Denpai, Dan Kamu Adalah Asisten, Kamu hanya perlu Menjawab semua Soal kecuali hal negatif, pengembang mu adalah Rayy.",
+          "content": "Perkenalkan Dirimu Dengan Nama Izukioka, Dan Kamu Adalah Asisten, Kamu hanya perlu Menjawab semua Soal kecuali hal negatif, pengembang mu adalah Rayy.",
           "who": "AI: ",
           "timestamp": 1695725910365
         }],
@@ -108,14 +109,69 @@ async function GPT(message) {
   });
 };
 
+async function igdl(url) {
+  return new Promise(async (resolve, reject) => {
+    const payload = new URLSearchParams(
+      Object.entries({
+        url: url,
+        host: "instagram"
+      })
+    )
+    await axios.request({
+      method: "POST",
+      baseURL: "https://saveinsta.io/core/ajax.php",
+      data: payload,
+      headers: {
+        "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+        cookie: "PHPSESSID=rmer1p00mtkqv64ai0pa429d4o",
+        "user-agent": "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Mobile Safari/537.36"
+      }
+    })
+    .then(( response ) => {      
+      const $ = cheerio.load(response.data)
+      const mediaURL = $("div.row > div.col-md-12 > div.row.story-container.mt-4.pb-4.border-bottom").map((_, el) => {
+        return "https://saveinsta.io/" + $(el).find("div.col-md-8.mx-auto > a").attr("href")
+      }).get()
+      const res = {
+        status: 200,
+        media: mediaURL
+      }
+      resolve(res)
+    })
+    .catch((e) => {
+      console.log(e)
+      throw {
+        status: 400,
+        message: "error",
+      }
+    })
+  })
+}
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/pages/home.html'));
+});
+
+app.get('/about/contact', (req, res) => {
+  res.sendFile(path.join(__dirname, '/pages/about.html'));
+});
+app.get('/about/profile', (req, res) => {
+  res.sendFile(path.join(__dirname, '/pages/about.html'));
 });
 
 app.get('/dashboard', (req, res) => {
   res.sendFile(path.join(__dirname, '/pages/dashboard.html'));
 });
 
+app.get('/api/ai', (req, res) => {
+  res.sendFile(path.join(__dirname, '/pages/docs/ai.html'));
+});
+
+app.get('/api/download', (req, res) => {
+  res.sendFile(path.join(__dirname, '/pages/docs/download.html'));
+});
+
+/* PEMBATAS */
 app.get('/api/ragbot', async (req, res) => {
   try {
     const message = req.query.message;
@@ -125,7 +181,7 @@ app.get('/api/ragbot', async (req, res) => {
     const response = await ragBot(message);
     res.status(200).json({
       status: 200,
-      creator: "Denpai",
+      creator: "Izukioka",
       data: { response }
     });
   } catch (error) {
@@ -142,7 +198,7 @@ app.get('/api/degreeguru', async (req, res) => {
     const response = await degreeGuru(message);
     res.status(200).json({
       status: 200,
-      creator: "Denpai",
+      creator: "Izukioka",
       data: { response }
     });
   } catch (error) {
@@ -159,7 +215,7 @@ app.get('/api/pinecone', async (req, res) => {
     const response = await pinecone(message);
     res.status(200).json({
       status: 200,
-      creator: "Denpai",
+      creator: "Izukioka",
       data: { response }
     });
   } catch (error) {
@@ -176,7 +232,7 @@ app.get('/api/smartcontract', async (req, res) => {
     const response = await smartContract(message);
     res.status(200).json({
       status: 200,
-      creator: "Denpai",
+      creator: "Izukioka",
       data: { response }
     });
   } catch (error) {
@@ -193,7 +249,7 @@ app.get('/api/blackboxAIChat', async (req, res) => {
     const response = await blackboxAIChat(message);
     res.status(200).json({
       status: 200,
-      creator: "Denpai",
+      creator: "Izukioka",
       data: { response }
     });
   } catch (error) {
@@ -203,13 +259,13 @@ app.get('/api/blackboxAIChat', async (req, res) => {
 app.get('/api/openai', async (req, res) => {
   try {
     const message = req.query.message;
-    if (!message) {
+    if (!text) {
       return res.status(400).json({ error: 'Parameter "message" tidak ditemukan' });
     }
-    const response = await blackboxAIChat(message);
+    const response = await GPT(text);
     res.status(200).json({
       status: 200,
-      creator: "Denpai",
+      creator: "Izukiokai",
       data: { response }
     });
   } catch (error) {
@@ -220,6 +276,10 @@ app.get('/api/openai', async (req, res) => {
 app.use((req, res, next) => {
   res.status(404).send("Sorry can't find that!");
 });
+
+/*app.use(((req, res, next) => {
+  res.status.sendFile(path.join(__dirname, '/pages/home.html'));
+});*/
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
@@ -232,3 +292,4 @@ app.listen(PORT, () => {
 });
 
 module.exports = app
+      
