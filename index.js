aa6cc4c0fb2360a84623f7b4ded5ca2c53db01b3
Takashi Izuki <148468601+izukioka@users.noreply.github.com>
2024-06-04 20:46:57 +0700

    Update index.js

--- index.js
+++ index.js
@@ -109,6 +109,50 @@ async function GPT(text) {
   });
 };
 
+async function ytdown2(url) {
+  return new Promise((resolve, reject) => {
+    try {
+      const id = ytdl.getVideoID(url)
+      const yutub = ytdl.getInfo(`https://www.youtube.com/watch?v=${id}`)
+      .then((data) => {
+        let pormat = data.formats
+        let audio = []
+        let video = []
+        for (let i = 0; i < pormat.length; i++) {
+         if (pormat[i].container == 'mp4' && pormat[i].hasVideo == true && pormat[i].hasAudio == true) {
+            let vid = pormat[i]
+            video.push(vid.url)
+          }
+          if (pormat[i].mimeType == 'audio/webm; codecs=\"opus\"') {
+            let aud = pormat[i]
+            audio.push(aud.url)
+          }
+        }
+        const title = data.player_response.microformat.playerMicroformatRenderer.title.simpleText
+        const thumb = data.player_response.microformat.playerMicroformatRenderer.thumbnail.thumbnails[0].url
+        const channel = data.player_response.microformat.playerMicroformatRenderer.ownerChannelName
+        const views = data.player_response.microformat.playerMicroformatRenderer.viewCount
+        const published = data.player_response.microformat.playerMicroformatRenderer.publishDate
+        
+        const result = {
+          title: title,
+          thumb: thumb,
+          channel: channel,
+          published: published,
+          views: views,
+          url: video[0],
+          creator: "Xynz Or Vanz"
+        }
+        return(result)
+      })
+      resolve(yutub)
+    } catch (error) {
+        reject(error);
+      }
+      console.log(error)
+  })
+}
+
 async function igdl(url) {
   return new Promise(async (resolve, reject) => {
     const payload = new URLSearchParams(
